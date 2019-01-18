3dd067a290c31e1faf61f493d8ebed14fb64720d
cedricfrancoys <cedricfrancoys@gmail.com>
2019-01-18 17:38:11 +0100

    added netdata console

--- docker/console_start.sh
+++ docker/console_start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 sudo docker stop portainer && sudo docker rm portainer
-sudo docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer 
+sudo docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker run -d --name=netdata \
+  -p 19999:19999 \
+  -v /proc:/host/proc:ro \
+  -v /sys:/host/sys:ro \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  --cap-add SYS_PTRACE \
+  --security-opt apparmor=unconfined \
+  netdata/netdata
