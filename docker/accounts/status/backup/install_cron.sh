49155a07740ad33d3861703e51fc570a50e4106a
cedricfrancoys <cedricfrancoys@gmail.com>
2019-01-28 10:44:46 +0100

    added cleanup.php

--- docker/accounts/status/backup/install_cron.sh
+++ docker/accounts/status/backup/install_cron.sh
@@ -7,6 +7,7 @@ then
     . ./../../.env
 
     touch /var/spool/cron/crontabs/root
+    (crontab -l ; echo "0 0 * * * /usr/bin/php /home/docker/backup/ftp/cleanup.php $DOMAIN_NAME")| crontab -
     (crontab -l ; echo "0 */6 * * * /home/docker/backup/backup.sh $DOMAIN_NAME")| crontab -
     (crontab -l ; echo "30 */6 * * * /usr/bin/php /home/docker/backup/ftp/export.php $DOMAIN_NAME")| crontab -
 
