40513141c8e00dd8546b7674684d55e4c4e51591
cedricfrancoys <cedricfrancoys@gmail.com>
2019-04-03 15:05:07 +0200

    allowed dynamic DB container name

--- docker/accounts/wordpress/init.sh
+++ docker/accounts/wordpress/init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
 # Build docked-wordpress image
 chmod +x /home/docker/images/docked-wordpress/build.sh
 cd /home/docker/images/docked-wordpress/
 ./build.sh
-
 
 # assign ownership to user and www-data (group)
 chown -R $USERNAME:www-data /home/$USERNAME/www
