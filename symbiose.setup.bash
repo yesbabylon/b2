ff38ffa254a6f280df8ff99661bc879a35b47e06
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-19 14:45:52 +0200

    Update symbiose.setup.bash

--- symbiose.setup.bash
+++ symbiose.setup.bash
@@ -2,7 +2,7 @@ if [ "$WITH_SB" = true ]; then
   print_color "yellow" "Clone an setup of Symbiose started..."
   docker exec -ti "$USERNAME" bash -c "
   mv packages packages-core
-  yes | git clone -b $EQ_VERSION https://github.com/yesbabylon/symbiose.git packages
+  yes | git clone -b dev-2.0 https://github.com/yesbabylon/symbiose.git packages
   mv packages-core/{core,demo} packages/
   rm -rf packages-core
   "
