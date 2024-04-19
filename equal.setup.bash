c6f11821483074f74273918490afcdc019714830
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-19 12:16:59 +0200

    Update equal.setup.bash

--- equal.setup.bash
+++ equal.setup.bash
@@ -40,26 +40,47 @@ replace_placeholders_for_docker_compose() {
 
 replace_placeholders_for_docker_compose
 
+print_color "yellow" "Building and starting the containers..."
 docker-compose build
 docker-compose up -d
 
+print_color "yellow" "Waiting 15 seconds for being sure than containers are correctly initialised."
+sleep 15
+
+print_color "yellow" "Installation of wget package"
+docker exec -ti $USERNAME bash -c "
+apt update
+apt install wget
+"
+
+print_color "yellow" "Clone of Equal started..."
+docker exec -ti $USERNAME bash -c "
+yes | git clone -b dev-2.0 https://github.com/AlexisVS/equal.git .
+"
+print_color "cyan" "Clone of eQual framework done."
+
+print_color "yellow" "Generation of config/config.json"
+docker exec -ti "$USERNAME" bash -c "
+ sh ./equal.run --do=config_generate --dbms=MYSQL --db_host=$DB_HOSTNAME --db_port=$DB_PORT --db_name=$DB_NAME --db_username=$APP_USERNAME --db_password=$APP_PASSWORD --app_url=$USERNAME --store=true
+"
+
+print_color "yellow" "Get cpublic/assets/env/config.json file from the repository..."
+# wget https://raw.githubusercontent.com/yesbabylon/b2/master/eQualPress_template/config/config.json -O config/config.json
+docker exec -ti $USERNAME bash -c "
+wget https://raw.githubusercontent.com/yesbabylon/b2/master/eQualPress_template/public/assets/env/config.json -O public/assets/env/config.json
+"
 
 cd /home/"$USERNAME"/www || exit
 
-print_color "yellow" "Clone of Equal started..."
-yes | git clone -b dev-2.0 https://github.com/equalframework/equal.git .
-print_color "cyan" "Clone of eQual framework done."
-
-print_color "yellow" "Get config files from the repository..."
-wget https://raw.githubusercontent.com/yesbabylon/b2/master/eQualPress_template/config/config.json -O /home/"$USERNAME"/www/config/config.json
-wget https://raw.githubusercontent.com/yesbabylon/b2/master/eQualPress_template/public/assets/env/config.json -O /home/"$USERNAME"/www/public/assets/env/config.json
+print_color "yellow" "Waiting 10 seconds for being sure than the volume is synced with the filesystem"
+sleep 10
 
 print_color "yellow" "Replacing placeholders in files..."
-replace_placeholders() {
+replace_config_placeholders() {
     # Replace placeholders with computed values
     for key in DB_PORT PHPMYADMIN_PORT EQ_PORT DB_NAME DB_HOSTNAME PMA_HOSTNAME; do
         value=$(eval echo \$$key)
-        for file in docker-compose.yml config/config.json public/assets/env/config.json; do
+        for file in public/assets/env/config.json; do
             # Replace placeholder with value
             sed -i "s/{$key}/$value/g" "$file"
         done
@@ -68,27 +89,18 @@ replace_placeholders() {
     # Read .env file and replace placeholders with values
     # shellcheck disable=SC2154
     while IFS='=' read -r key value; do
-        for file in docker-compose.yml config/config.json public/assets/env/config.json; do
+        for file in public/assets/env/config.json; do
             # Replace placeholder with value
             sed -i "s/{$key}/$value/g" "$file"
         done
     done < "$script_dir"/.env
 }
 
-replace_placeholders
-
-
-print_color "yellow" "Building and starting the containers..."
-cd /home/"$USERNAME"/www || exit
-
-
-print_color "yellow" "Waiting 15 seconds for the containers starting..."
-sleep 15
+replace_config_placeholders
 
 # print_color "yellow" "Move config/config.json file"
 # print_color "yellow" "Move public/assets/env/config.json file"
 # docker exec -ti "$USERNAME" bash -c "
-# sh ./equal.run --do=config_generate --dbms=MYSQL --db_host=$DB_HOSTNAME --db_port=$DB_PORT --db_name=$DB_NAME --db_username=$APP_USERNAME --db_password=$APP_PASSWORD --app_url=$USERNAME --store=true
 # sh ./equal.run --get=envinfo-temp 
 # "
 
