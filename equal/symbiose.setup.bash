a72fa3e9fc532b2d9fa013b267db9196bf748e78
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-22 16:57:21 +0200

    Update symbiose.setup.bash

--- equal/symbiose.setup.bash
+++ equal/symbiose.setup.bash
@@ -1,7 +1,26 @@
-  print_color "yellow" "Clone an setup of Symbiose started..."
-  docker exec -ti "$USERNAME" bash -c "
-  mv packages packages-core
-  yes | git clone -b dev-2.0 https://github.com/yesbabylon/symbiose.git packages
-  mv packages-core/{core,demo} packages/
-  rm -rf packages-core
-  "
+# Function to print colored text
+print_color() {
+    local color="$1"
+    local text="$2"
+    case "$color" in
+        "black") echo -e "\033[1;30m$text\033[0m" ;;
+        "red") echo -e "\033[1;31m$text\033[0m" ;;
+        "green") echo -e "\033[1;32m$text\033[0m" ;;
+        "yellow") echo -e "\033[1;33m$text\033[0m" ;;
+        "blue") echo -e "\033[1;34m$text\033[0m" ;;
+        "magenta") echo -e "\033[1;35m$text\033[0m" ;;
+        "cyan") echo -e "\033[1;36m$text\033[0m" ;;
+        "white") echo -e "\033[1;37m$text\033[0m" ;;
+        "bgred") echo -e "\033[1;37;41m$text\033[0m" ;;
+        "bggreen") echo -e "\033[1;37;42m$text\033[0m" ;;
+        *) echo "Invalid color" >&2 ;;
+    esac
+}
+  
+print_color "yellow" "Clone an setup of Symbiose started..."
+docker exec -ti "$USERNAME" bash -c "
+mv packages packages-core
+yes | git clone -b dev-2.0 https://github.com/yesbabylon/symbiose.git packages
+mv packages-core/{core,demo} packages/
+rm -rf packages-core
+"
