02290589695cd5ae45e2137f739644dbfcf34b79
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-22 15:43:16 +0200

    Update init.bash

--- equal/init.bash
+++ equal/init.bash
@@ -83,92 +83,85 @@ then
     source .env
     set +o allexport
 
-    if [ -z "$USERNAME" ]
-    then
-        print_color "bgred" "A file named .env is expected and should contain following vars definition:"
-        print_color "bgred" "USERNAME={domain-name-as-user-name}"
-        print_color "bgred" "PASSWORD={user-password}"
-    else
-        if [ ${#USERNAME} -gt 32 ]
-        then 
-            print_color "bgred" "Error: username must be max 32 chars long"
-            exit 1
-        fi
-
-        print_color "yellow" "Create a new user"
-        adduser --force-badname --disabled-password --gecos ",,," "$USERNAME"
-        echo "$USERNAME:$PASSWORD" | sudo chpasswd
-
-        print_color "yellow" "Create directories for backup and replication"
-        mkdir /home/"$USERNAME"
-        mkdir /home/"$USERNAME"/import
-        mkdir /home/"$USERNAME"/export
-
-        print_color "yellow" "directories for dealing with status"
-        cp -r /home/docker/accounts/status /home/"$USERNAME"/status
-
-        print_color "yellow" "Set the home directory of the new user (FTP access)"
-        mkdir -p /home/"$USERNAME"/www
-
-        sudo usermod -d /home/"$USERNAME"/www "$USERNAME"
-
-        print_color "yellow" "Create a directory for maintenance switch"
-        mkdir /srv/docker/nginx/html/"$USERNAME"
-
-        print_color "yellow" "Add write permission to group over the www directory of the user"
-        chmod g+w -R /home/"$USERNAME"/www
-
-        print_color "yellow" "Restart SFTP service (to enable ftp login at user home)"
-        sudo systemctl restart vsftpd
-
-        print_color "yellow" "Add account to docker group"
-        sudo usermod -a -G docker "$USERNAME"
-
-        print_color "yellow" "Define ssh-login as shell for user account"
-        sudo chsh -s /usr/local/bin/ssh-login "$USERNAME"
-
-        print_color "yellow" "Define a hash value with the first 5 characters of the md5sum of the username"
-        HASH_VALUE=$(printf "%.5s" "$(echo "$USERNAME" | md5sum | cut -d ' ' -f 1)")
-
-        print_color "yellow" "Define DB_HOST with the hash value"
-        export DB_HOSTNAME="db_$HASH_VALUE"
-
-        print_color "yellow" "Rename PHPMYADMIN_SERVICE_NAME with the hash value"
-        export PMA_HOSTNAME="phpmyadmin_$HASH_VALUE"
-
-        print_color "yellow" "Get the number of directories in /home"
-        # shellcheck disable=SC2010
-        number_of_directories=$(ls -l /home | grep -c ^d)
-
-        print_color "yellow" "Define DB_PORT with the number of directories in /home"
-        # shellcheck disable=SC2004
-        export DB_PORT=$(( 3306 - 1 + $number_of_directories ))
-
-        print_color "yellow" "Define PHPMYADMIN_PORT with the number of directories in /home"
-        # shellcheck disable=SC2004
-        export PMA_PORT=$(( 8080 - 1 + $number_of_directories ))
-
-        print_color "yellow" "Define EQ_PORT with the number of directories in /home"
-        # shellcheck disable=SC2004
-        export EQ_PORT=$(( 80 - 1 + $number_of_directories ))
-
-        bash "$script_dir"/equal.setup.bash
-
-        if [ "$WITH_SB" = true ]; then
-            print_color "yellow" "Installation of Symbiose."
-            bash "$script_dir"/symbiose.setup.bash
-            print_color "yellow" "End of Symbiose installation."
-        fi
-
-        if [ "$WITH_WP" = true ]; then
-            print_color "yellow" "Installation of eQualPress."
-            wget https://raw.githubusercontent.com/eQualPress/equalpress/main/install.sh -O /home/"$USERNAME"/install.sh
-            chmod +x /home/"$USERNAME"/install.sh
-            /home/"$USERNAME"/install.sh
-            print_color "yellow" "End of eQualPress installation"
-            rm /home/"$USERNAME"/install.sh
-        fi
-
-        print_color "magenta" "Script setup completed successfully!"
+    if [ ${#USERNAME} -gt 32 ]
+    then 
+        print_color "bgred" "Error: username must be max 32 chars long"
+        exit 1
     fi
+
+    print_color "yellow" "Create a new user"
+    adduser --force-badname --disabled-password --gecos ",,," "$USERNAME"
+    echo "$USERNAME:$PASSWORD" | sudo chpasswd
+
+    print_color "yellow" "Create directories for backup and replication"
+    mkdir /home/"$USERNAME"
+    mkdir /home/"$USERNAME"/import
+    mkdir /home/"$USERNAME"/export
+
+    print_color "yellow" "directories for dealing with status"
+    cp -r /home/docker/accounts/status /home/"$USERNAME"/status
+
+    print_color "yellow" "Set the home directory of the new user (FTP access)"
+    mkdir -p /home/"$USERNAME"/www
+
+    sudo usermod -d /home/"$USERNAME"/www "$USERNAME"
+
+    print_color "yellow" "Create a directory for maintenance switch"
+    mkdir /srv/docker/nginx/html/"$USERNAME"
+
+    print_color "yellow" "Add write permission to group over the www directory of the user"
+    chmod g+w -R /home/"$USERNAME"/www
+
+    print_color "yellow" "Restart SFTP service (to enable ftp login at user home)"
+    sudo systemctl restart vsftpd
+
+    print_color "yellow" "Add account to docker group"
+    sudo usermod -a -G docker "$USERNAME"
+
+    print_color "yellow" "Define ssh-login as shell for user account"
+    sudo chsh -s /usr/local/bin/ssh-login "$USERNAME"
+
+    print_color "yellow" "Define a hash value with the first 5 characters of the md5sum of the username"
+    HASH_VALUE=$(printf "%.5s" "$(echo "$USERNAME" | md5sum | cut -d ' ' -f 1)")
+
+    print_color "yellow" "Define DB_HOST with the hash value"
+    export DB_HOSTNAME="db_$HASH_VALUE"
+
+    print_color "yellow" "Rename PHPMYADMIN_SERVICE_NAME with the hash value"
+    export PMA_HOSTNAME="phpmyadmin_$HASH_VALUE"
+
+    print_color "yellow" "Get the number of directories in /home"
+    # shellcheck disable=SC2010
+    number_of_directories=$(ls -l /home | grep -c ^d)
+
+    print_color "yellow" "Define DB_PORT with the number of directories in /home"
+    # shellcheck disable=SC2004
+    export DB_PORT=$(( 3306 - 1 + $number_of_directories ))
+
+    print_color "yellow" "Define PHPMYADMIN_PORT with the number of directories in /home"
+    # shellcheck disable=SC2004
+    export PMA_PORT=$(( 8080 - 1 + $number_of_directories ))
+
+    print_color "yellow" "Define EQ_PORT with the number of directories in /home"
+    # shellcheck disable=SC2004
+    export EQ_PORT=$(( 80 - 1 + $number_of_directories ))
+
+    bash "$script_dir"/equal.setup.bash
+
+    if [ "$WITH_SB" = true ]; then
+        print_color "yellow" "Installation of Symbiose."
+        bash "$script_dir"/symbiose.setup.bash
+        print_color "yellow" "End of Symbiose installation."
+    fi
+
+    if [ "$WITH_WP" = true ]; then
+        print_color "yellow" "Installation of eQualPress."
+        wget https://raw.githubusercontent.com/eQualPress/equalpress/main/install.sh -O /home/"$USERNAME"/install.sh
+        chmod +x /home/"$USERNAME"/install.sh
+        /home/"$USERNAME"/install.sh
+        print_color "yellow" "End of eQualPress installation"
+        rm /home/"$USERNAME"/install.sh
+    fi
+
+    print_color "magenta" "Script setup completed successfully!"
 fi
