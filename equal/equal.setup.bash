c08cfee89b37e6f63d61e1eccf66bc1899a0156d
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-25 16:18:12 +0200

    Update equal.setup.bash

--- equal/equal.setup.bash
+++ equal/equal.setup.bash
@@ -47,7 +47,7 @@ print_color "yellow" "Clone of Equal started..."
 docker exec -ti "$USERNAME" bash -c "
 apt update
 apt install wget
-yes | git clone -b dev-2.0 https://github.com/equalframework/equal.git .
+yes | git clone -b dev-2.0 https://github.com/AlexisVS/equal.git .
 "
 print_color "cyan" "Clone of eQual framework done."
 
