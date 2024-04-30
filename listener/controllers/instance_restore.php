b4ab4a871d050971e284b2b4c90ac88c024ec858
AlexisVS <alexis.vansan1440@gmail.com>
2024-04-30 15:29:52 +0200

    update

--- listener/controllers/instance_restore.php
+++ listener/controllers/instance_restore.php
@@ -2,7 +2,7 @@
 
 /**
  * Restore an instance.
- * ! Not sure
+ * TODO: Need to change backup scripts logic for export.php and restore.php
  *
  * @param array{instance: string, backup_id: string} $data
  * @return array{code: int, message: string}
@@ -12,11 +12,20 @@ function instance_restore(array $data): array
     $status_code = 201;
     $message = '';
 
-    if (!isset($data['instance']) || !is_string($data['instance']) || strlen($data['instance']) === 0) {
+    if (!isset($data['instance']) || !isset($data['backup_id'])) {
         $status_code = 400;
-    } else {
-        exec('sh /home/docker/backups/restore.sh ' . $data['instance']);
+        $message = 'Bad Request';
     }
+
+    // 1. active maintenance mode
+    exec('sh /home/' . $data['instance'] . '/status/maintenance/enabled.sh');
+    // 2. create backup
+    exec('sh /root/b2/backup/backup.sh ' . $data['instance']);
+    // 3. export backups
+    exec('user/bin/php /root/b2/backup/export.php ' . $data['instance']);
+    // 3. restore backup with $data['backup_id']
+    // 4. deactivate maintenance mode
+    exec('sh /home/' . $data['instance'] . '/status/maintenance/disable.sh');
 
     return [
         'code' => $status_code,
