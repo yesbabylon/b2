675841dc23435cd58d95f6acf236db96894e6e2d
AlexisVS <alexis.vansan1440@gmail.com>
2024-04-30 15:57:07 +0200

    updated instance_delete.php

--- listener/controllers/instance_delete.php
+++ listener/controllers/instance_delete.php
@@ -12,17 +12,25 @@ function instance_delete(array $data): array
     $status_code = 201;
     $message = '';
 
+    $pwd = getcwd();
+
+    // going to /home/$data['instance']
+    chdir('/home/' . $data['instance']);
+
     if (!isset($data['instance']) || !is_string($data['instance']) || strlen($data['instance']) === 0) {
         $status_code = 400;
     } else {
         // Stop and remove the instance with docker-compose to /home/$data['instance']
-        exec('docker-compose -v -f /home/' . $data['instance'] . '/docker-compose.yml down');
+        exec('docker-compose down -v');
 
         // Rename the instance directory to /home/$data['instance']_deleted
         exec('mv /home/' . $data['instance'] . ' /home/' . $data['instance'] . '_deleted');
 
         // Remove all files and directories in /home/$data['instance']_deleted but keep the directory
         exec('rm -rf /home/' . $data['instance'] . '_deleted/*');
+
+        // return to the previous directory
+        chdir($pwd);
     }
 
     return [
