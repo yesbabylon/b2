8a1960f4e1417306186dd1bb97005e8c3628133a
Lucas <lucas@yesbabylon.com>
2024-12-02 11:25:03 +0100

    Delete instance

--- listener/controllers/instance/delete.php
+++ listener/controllers/instance/delete.php
@@ -7,30 +7,25 @@
  * @return array{code: int, body: string}
  */
 function instance_delete(array $data): array {
-    if(!isset($data['instance']) || !is_string($data['instance']) || strlen($data['instance']) === 0) {
+    if(
+        !isset($data['instance']) || !is_string($data['instance']) || strlen($data['instance']) === 0
+        || preg_match('/^(?!\-)(?:[a-zA-Z0-9\-]{1,63}\.)+[a-zA-Z]{2,}$/', $data['USERNAME']) === 0
+        || !is_dir($data['instance'])
+    ) {
         throw new InvalidArgumentException("invalid_instance", 400);
     }
 
-    // Get the current working directory
-    $pwd = getcwd();
-
-    // going to /home/$data['instance']
-    chdir('/home/' . $data['instance']);
-
     // Stop and remove the instance with docker compose to /home/$data['instance']
-    exec('docker compose down -v');
+    exec('docker compose -f /home/'.$data['instance'].'/docker-compose.yml down -v');
 
     // Rename the instance directory to /home/$data['instance']_deleted
-    exec('mv /home/' . $data['instance'] . ' /home/' . $data['instance'] . '_deleted');
+    exec('mv /home/'.$data['instance'].' /home/'.$data['instance'].'_deleted');
 
     // Remove all files and directories in /home/$data['instance']_deleted but keep the directory
-    exec('rm -rf /home/' . $data['instance'] . '_deleted/*');
-
-    // return to the previous directory
-    chdir($pwd);
+    exec('rm -rf /home/'.$data['instance'].'_deleted/*');
 
     // Delete linux user
-    exec('userdel -f ' . $data['instance']);
+    exec('userdel -f '.$data['instance']);
 
     return [
         'code' => 200,
