3eb8c400b8c7a515b4041f586fe371be9a32944f
Lucas <lucas@yesbabylon.com>
2024-12-03 09:23:36 +0100

    add more security to instance_backup method

--- listener/controllers/instance/backup.php
+++ listener/controllers/instance/backup.php
@@ -12,35 +12,37 @@ function instance_backup(array $data): array {
         throw new InvalidArgumentException("missing_instance", 400);
     }
 
-    if(in_array($data['instance'], ['docker', 'ubuntu'])) {
+    $instance = escapeshellarg($data['instance']);
+
+    if(in_array($instance, ['..', '.', 'docker', 'ubuntu'])) {
         throw new InvalidArgumentException("invalid_instance", 400);
     }
 
-    if(!file_exists('/home/'.$data['instance']) || !is_dir('/home/'.$data['instance'])) {
+    if(!file_exists('/home/'.$instance) || !is_dir('/home/'.$instance)) {
         throw new \Exception("instance_not_found", 404);
     }
 
     // Remove old export, if any
-    exec("rm -rf /home/${$data['instance']}/export");
-    exec("mkdir /home/${$data['instance']}/export");
+    exec("rm -rf /home/$instance/export");
+    exec("mkdir /home/$instance/export");
 
     // Backup
     $volume_name = str_replace('.', '', $data['instance']).'_db_data';
 
     $to_export = [
         "/var/lib/docker/volumes/$volume_name/_data",
-        "/home/${$data['instance']}/.env",
-        "/home/${$data['instance']}/docker-compose.yml",
-        "/home/${$data['instance']}/php.ini",
-        "/home/${$data['instance']}/mysql.cnf",
-        "/home/${$data['instance']}/www"
+        "/home/$instance/.env",
+        "/home/$instance/docker-compose.yml",
+        "/home/$instance/php.ini",
+        "/home/$instance/mysql.cnf",
+        "/home/$instance/www"
         // TODO: Handle SSL/TLS Certificates
     ];
 
     $timestamp = date('YmdHis');
     $to_export_str = implode('', $to_export);
 
-    exec("tar -cvzf /home/${$data['instance']}/export/backup-$timestamp.tar.gz $to_export_str");
+    exec("tar -cvzf /home/$instance/export/backup-$timestamp.tar.gz $to_export_str");
 
     return [
         'code' => 201,
