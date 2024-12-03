6b14cfb1c5252388b5c619141f57177443f017aa
Lucas <lucas@yesbabylon.com>
2024-12-03 09:35:45 +0100

    fix issues escapeshellarg

--- listener/controllers/instance/backup.php
+++ listener/controllers/instance/backup.php
@@ -12,37 +12,40 @@ function instance_backup(array $data): array {
         throw new InvalidArgumentException("missing_instance", 400);
     }
 
-    $instance = escapeshellarg($data['instance']);
-
-    if(in_array($instance, ['..', '.', 'docker', 'ubuntu'])) {
+    if(
+        in_array($data['instance'], ['..', '.', 'docker', 'ubuntu'])
+        || $data['instance'] !== basename($data['instance'])
+    ) {
         throw new InvalidArgumentException("invalid_instance", 400);
     }
 
-    if(!file_exists('/home/'.$instance) || !is_dir('/home/'.$instance)) {
+    if(!file_exists('/home/'.$data['instance']) || !is_dir('/home/'.$data['instance'])) {
         throw new \Exception("instance_not_found", 404);
     }
 
+    $instance_escaped = escapeshellarg($data['instance']);
+
     // Remove old export, if any
-    exec("rm -rf /home/$instance/export");
-    exec("mkdir /home/$instance/export");
+    exec("rm -rf /home/$instance_escaped/export");
+    exec("mkdir /home/$instance_escaped/export");
 
     // Backup
     $volume_name = str_replace('.', '', $data['instance']).'_db_data';
 
     $to_export = [
         "/var/lib/docker/volumes/$volume_name/_data",
-        "/home/$instance/.env",
-        "/home/$instance/docker-compose.yml",
-        "/home/$instance/php.ini",
-        "/home/$instance/mysql.cnf",
-        "/home/$instance/www"
+        "/home/$instance_escaped/.env",
+        "/home/$instance_escaped/docker-compose.yml",
+        "/home/$instance_escaped/php.ini",
+        "/home/$instance_escaped/mysql.cnf",
+        "/home/$instance_escaped/www"
         // TODO: Handle SSL/TLS Certificates
     ];
 
     $timestamp = date('YmdHis');
     $to_export_str = implode('', $to_export);
 
-    exec("tar -cvzf /home/$instance/export/backup-$timestamp.tar.gz $to_export_str");
+    exec("tar -cvzf /home/$instance_escaped/export/backup-$timestamp.tar.gz $to_export_str");
 
     return [
         'code' => 201,
