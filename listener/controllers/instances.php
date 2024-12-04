c1beb00e4f62e5731594ffce087f7b41a39605e5
Lucas <lucas@yesbabylon.com>
2024-12-04 11:04:43 +0100

    enhance instances endpoint

--- listener/controllers/instances.php
+++ listener/controllers/instances.php
@@ -3,16 +3,20 @@
 /**
  * Returns list of instances present on the host, separates active and deleted instances.
  *
+ * @param array{with_deleted?: bool} $data
  * @return array{
  *     code: int,
  *     body: array{
- *         active_instances: string[],
- *         deleted_instances: string[]
+ *         instances: string[]
  *     }
  * }
  * @throws Exception
  */
-function instances(): array {
+function instances(array $data): array {
+    if(isset($data['with_deleted']) && !is_bool($data['with_deleted'])) {
+        throw new InvalidArgumentException("invalid_with_deleted", 400);
+    }
+
     // Get the list of instances
     $directories = scandir('/home');
     if($directories === false) {
@@ -22,20 +26,18 @@ function instances(): array {
     // Remove the '.' and '..' and 'ubuntu' and 'docker' entries
     $directories = array_values(array_diff($directories, ['.', '..', 'ubuntu', 'docker']));
 
-    // Separate active and deleted instances
-    $active_instances = [];
-    $deleted_instances = [];
+    // Add also deleted instances
+    $with_deleted = $data['with_deleted'] ?? false;
+
+    $instances = [];
     foreach($directories as $instance) {
-        if(strpos($instance, '_deleted') === false) {
-            $active_instances[] = $instance;
-        }
-        else {
-            $deleted_instances[] = $instance;
+        if($with_deleted || strpos($instance, '_deleted') === false) {
+            $instances[] = $instance;
         }
     }
 
     return [
         'code' => 200,
-        'body' => compact('active_instances', 'deleted_instances')
+        'body' => compact('instances')
     ];
 }
