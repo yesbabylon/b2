80edbaa8e3345840999f7a612488a31719b8242f
Lucas <lucas@yesbabylon.com>
2024-12-05 16:31:57 +0100

    fix send backup controller

--- listener/controllers/instance/send-backup.php
+++ listener/controllers/instance/send-backup.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
- * Sends a backup file to a backup host.
- * ! Note sure
+ * Sends a backup file to the configured backup host.
  *
- * @param array{instance: string, backup_filename: string} $data
+ * @param array{instance: string, backup_id: string} $data
  * @return array{code: int, body: string}
  * @throws Exception
  */
@@ -24,47 +23,76 @@ function instance_send_backup(array $data): array {
         throw new \Exception("instance_not_found", 404);
     }
 
-    if(!isset($data['backup_filename'])) {
-        throw new InvalidArgumentException('missing_backup_filename', 400);
+    if(!isset($data['backup_id'])) {
+        throw new InvalidArgumentException("missing_backup_id", 400);
     }
 
-    $backup_host = 'http://backup-host';
-
-    // While the token is not available, wait for it 60 seconds and try again.
-    while($ftp_credentials = file_get_contents($backup_host . '/token')) {
-        sleep(60);
+    $backup_file = '/home/'.$data['instance'].'/export/backup_'.$data['backup_id'].'.tar.gz';
+    if(!file_exists($backup_file)) {
+        throw new \Exception("backup_not_found", 404);
     }
 
-    ['username' => $username, 'password' => $password] = $ftp_credentials['message']['credentials'];
-
-    // Connect to the backup host
-    $ftp_connection = ftp_connect($backup_host);
-
-    // Login to the backup host
-    if(!$ftp_connection || !ftp_login($ftp_connection, $username, $password)) {
-        throw new Exception('Server Error while connecting to backup host', 500);
+    $backup_host_url = getenv('BACKUP_HOST_URL') ?? false;
+    if(empty($backup_host_url)) {
+        throw new Exception("BACKUP_HOST_URL_not_configured", 500);
     }
 
-    $backup_file = "/home/${$data['instance']}/export/${$data['backup_filename']}";
-    if(!ftp_put($ftp_connection, $data['backup_filename'], $backup_file, FTP_BINARY)) {
-        throw new Exception('error_while_sending_backup', 500);
+    $backup_host_ftp = getenv('BACKUP_HOST_FTP') ?? false;
+    if(empty($backup_host_ftp)) {
+        throw new Exception("BACKUP_HOST_FTP_not_configured", 500);
     }
 
-    ftp_close($ftp_connection);
-
-    $jwt = file_get_contents('/home/status/jwt.txt');
-
-    // Release the token on the backup host
-    file_get_contents("$backup_host/token-release", false, stream_context_create([
+    // Create token create request
+    $post_data = http_build_query(['instance' => $data['backup_id']]);
+    $options = [
         'http' => [
-            'method'    => 'POST',
-            'header'    => 'Content-Type: application/json',
-            'content'   => json_encode([
-                'JWT'       => $jwt,
-                'instance'  => $data['instance']
-            ])
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => $post_data
         ]
-    ]));
+    ];
+    $context = stream_context_create($options);
+
+    // Send create token request
+    $response = file_get_contents($backup_host_url.'/token/create', false, $context);
+
+    if ($response === false) {
+        throw new Exception("error_while_asking_for_token", 500);
+    }
+
+    // Get token and created user ftp credentials
+    ['token' => $token, 'credentials' => $ftp_credentials] = json_decode($response, true);
+
+    $ftp_connection_id = ftp_connect($backup_host_ftp);
+    if(!$ftp_connection_id) {
+        throw new Exception("could_not_connect_to_ftp_server", 500);
+    }
+
+    if(!ftp_login($ftp_connection_id, $ftp_credentials['username'], $ftp_credentials['password'])) {
+        ftp_close($ftp_connection_id);
+        throw new Exception("could_not_log_in_ftp_server", 500);
+    }
+
+    if(!ftp_put($ftp_connection_id, basename($backup_file), $backup_file, FTP_BINARY)) {
+        ftp_close($ftp_connection_id);
+        throw new Exception("error_while_sending_backup_file", 500);
+    }
+
+    ftp_close($ftp_connection_id);
+
+    // Create token release request
+    $post_data = http_build_query(['instance' => $data['backup_id'], 'token' => $token]);
+    $options = [
+        'http' => [
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => $post_data
+        ]
+    ];
+    $context = stream_context_create($options);
+
+    // Send release token request
+    file_get_contents($backup_host_url.'/token/release', false, $context);
 
     return [
         'code' => 200,
