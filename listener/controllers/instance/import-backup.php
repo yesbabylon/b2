6a7e9d4e54f31bd39d96c969bb85f4252b7ad96a
Lucas <lucas@yesbabylon.com>
2024-12-06 12:09:11 +0100

    refacto

--- listener/controllers/instance/import-backup.php
+++ listener/controllers/instance/import-backup.php
@@ -3,7 +3,7 @@
 /**
  * Imports a backup file from the configured backup host.
  *
- * @param array{instance: string, backup_id?: string} $data
+ * @param array{instance: string, backup_id: string} $data
  * @return array{code: int, body: string}
  * @throws Exception
  */
@@ -24,7 +24,6 @@ function instance_import_backup(array $data): array {
     }
 
     if(!isset($data['backup_id'])) {
-        // TODO: handle get latest if no id provided
         throw new InvalidArgumentException("missing_backup_id", 400);
     }
 
