3c3a849dac25bac98cf432e644dc00677df99d5b
Lucas <lucas@yesbabylon.com>
2024-12-09 12:37:05 +0100

    handle give passphrase to export pgp command

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ gpg --batch --generate-key ./key-gen.conf
 rm ./key-gen.conf
 
 # Export private and public keys to pgp files
-gpg --output private-gpg-key.pgp --armor --export-secret-key "$GPG_EMAIL"
+echo "$GPG_PASSPHRASE" | gpg --batch --yes --output private-gpg-key.pgp --armor --export-secret-key "$GPG_EMAIL" --pinentry-mode loopback --passphrase-fd 0
 gpg --output public-gpg-key.pgp --armor --export "$GPG_EMAIL"
 
 
