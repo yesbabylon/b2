93f3ee69232ffb25c82d2ed50ecf96be3c6adc77
Lucas <lucas@yesbabylon.com>
2024-12-10 16:25:51 +0100

    remove not needed

--- install.sh
+++ install.sh
@@ -251,12 +251,6 @@ if ! crontab -l | grep -q "$PHP_SCRIPT"; then
     (crontab -l 2>/dev/null; echo "$CRON_CMD") | crontab -
 fi
 
-# Restart cron
-systemctl start cron
-
-# Enable cron at startup
-systemctl enable cron
-
 
 ###################
 ### Install F2B ###
