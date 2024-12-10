9611acc9443acaf39b3669284596bce893879fd4
Lucas <lucas@yesbabylon.com>
2024-12-10 14:34:00 +0100

    correct

--- cron.php
+++ cron.php
@@ -11,7 +11,7 @@ $cron_jobs = [
     // Add cron jobs here, like following example:
     // [
     //     'description'   => "Release expired backup tokens every 5 minutes.",
-    //     'crontab'       => '*/1 * * * *',
+    //     'crontab'       => '*/5 * * * *',
     //     'controller'    => 'release-expired-tokens'
     // ]
 ];
