4be58f1cdb1776b17f3e41ec1e48fbc86e43c823
Lucas <lucas@yesbabylon.com>
2024-12-12 11:39:02 +0100

    add routes to listener

--- listener.php
+++ listener.php
@@ -19,18 +19,20 @@ $request = [
 ];
 
 $allowed_routes = [
-    '/reboot',                  /* @link reboot() */
-    '/status',                  /* @link status() */
-    '/ip',                      /* @link ip() */
-    '/instances',               /* @link instances() */
-    '/instance/backup',         /* @link instance_backup() */
-    '/instance/backups',        /* @link instance_backups() */
-    '/instance/export-backup',  /* @link instance_export_backup() */
-    '/instance/import-backup',  /* @link instance_import_backup() */
-    '/instance/create',         /* @link instance_create() */
-    '/instance/delete',         /* @link instance_delete() */
-    '/instance/restore',        /* @link instance_restore() */
-    '/instance/status'          /* @link instance_status() */
+    '/reboot',                          /* @link reboot() */
+    '/status',                          /* @link status() */
+    '/ip',                              /* @link ip() */
+    '/instances',                       /* @link instances() */
+    '/instance/backup',                 /* @link instance_backup() */
+    '/instance/backups',                /* @link instance_backups() */
+    '/instance/export-backup',          /* @link instance_export_backup() */
+    '/instance/import-backup',          /* @link instance_import_backup() */
+    '/instance/create',                 /* @link instance_create() */
+    '/instance/delete',                 /* @link instance_delete() */
+    '/instance/restore',                /* @link instance_restore() */
+    '/instance/status',                 /* @link instance_status() */
+    '/instance/enable-maintenance',     /* @link instance_enable_maintenance() */
+    '/instance/disable-maintenance'     /* @link instance_disable_maintenance() */
 ];
 
 ['body' => $body, 'code' => $code] = handle_request($request, $allowed_routes);
