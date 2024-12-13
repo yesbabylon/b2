2e5b012facfedb513295147e9b40d14f42bc92c2
Lucas <lucas@yesbabylon.com>
2024-12-13 10:44:03 +0100

    remove test print

--- scripts/instance/create/create.bash
+++ scripts/instance/create/create.bash
@@ -145,8 +145,6 @@ printf "Env file created.\n"
 printf "Init eQual\n"
 bash "/root/b2/scripts/instance/create/init-equal.bash"
 
-exit 0
-
 symbiose=${symbiose:-false}
 equalpress=${equalpress:-false}
 
