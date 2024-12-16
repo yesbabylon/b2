e48ce8280c1c318ed10832d592efe21239d6f99a
Lucas <lucas@yesbabylon.com>
2024-12-16 09:47:18 +0100

    fix copy sql backup file to container before restore

--- controllers/instance/restore.php
+++ controllers/instance/restore.php
@@ -93,7 +93,9 @@ function instance_restore(array $data): array {
     // Restore database
     exec("cd $tmp_restore_dir && gunzip backup.sql.gz");
     exec("docker exec $db_hostname /usr/bin/mysql -u $db_backup_username --password=$db_backup_password -e \"DROP DATABASE IF EXISTS equal; CREATE DATABASE equal;\"");
-    exec("docker exec $db_hostname /usr/bin/mysql -u $db_backup_username --password=$db_backup_password equal < $tmp_restore_dir/backup.sql");
+    exec("docker cp /home/$instance/tmp_restore/backup.sql $db_hostname:/tmp/backup.sql");
+    exec("docker exec $db_hostname /usr/bin/mysql -u $db_backup_username --password=$db_backup_password equal < /tmp/backup.sql");
+    exec("docker exec $db_hostname rm /tmp/backup.sql");
 
     // Restore config
     exec("cd $tmp_restore_dir && tar -xvf config.tar");
