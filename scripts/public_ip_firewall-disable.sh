31b396608fcfb34a583c2af0b6a3f1c130a48f37
Cédric Françoys <cedricfrancoys@gmail.com>
2026-09-09 17:32:05 +0200

    updated

--- scripts/public_ip_firewall-disable.sh
+++ scripts/public_ip_firewall-disable.sh
@@ -3,20 +3,22 @@
 # File: public_ip_firewall-disable.sh
 # Description: Removes iptables rules configured for the failover IP.
 
-# Get the IP associated with veth0
-IP_ADDRESS=$(ip -4 addr show dev veth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+# Get the IPs associated with veth0
+PUBLIC_IPS=$(ip -4 addr show dev veth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-# Check if an IP is found
-if [ -z "$IP_ADDRESS" ]; then
-    echo "Error: No IP address found on veth0."
+# Check if any IP is found
+if [ -z "$PUBLIC_IPS" ]; then
+    echo "Error: No IP addresses found on veth0."
     exit 1
 fi
 
-echo "Removing iptables rules for IP: $IP_ADDRESS"
+for IP in $PUBLIC_IPS; do
+    echo "Removing iptables rules for IP: $IP"
 
-# Remove iptables rules
-iptables -D INPUT -d "$IP_ADDRESS" -p tcp --dport 80 -j ACCEPT
-iptables -D INPUT -d "$IP_ADDRESS" -p tcp --dport 443 -j ACCEPT
-iptables -D INPUT -d "$IP_ADDRESS" -j DROP
+    # Remove iptables rules
+    iptables -D INPUT -d "$IP" -p tcp --dport 80 -j ACCEPT
+    iptables -D INPUT -d "$IP" -p tcp --dport 443 -j ACCEPT
+    iptables -D INPUT -d "$IP" -j DROP
+done
 
 echo "iptables rules removed. Run 'sudo iptables -S' to check."
--- scripts/public_ip_firewall-enable.sh
+++ scripts/public_ip_firewall-enable.sh
@@ -3,20 +3,22 @@
 # File: public_ip_firewall-enable.sh
 # Description: Configures iptables to limit INPUT access to HTTP(S) on the failover IP.
 
-# Get the IP associated with veth0
-IP_ADDRESS=$(ip -4 addr show dev veth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+# Get the IPs associated with veth0
+PUBLIC_IPS=$(ip -4 addr show dev veth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-# Check if an IP is found
-if [ -z "$IP_ADDRESS" ]; then
-    echo "Error: No IP address found on veth0."
+# Check if any IP is found
+if [ -z "$PUBLIC_IPS" ]; then
+    echo "Error: No IP addresses found on veth0."
     exit 1
 fi
 
-echo "Configuring iptables for IP: $IP_ADDRESS"
+for IP in $PUBLIC_IPS; do
+    echo "Configuring iptables for IP: $IP"
 
-# Add iptables rules
-iptables -A INPUT -d "$IP_ADDRESS" -j DROP
-iptables -I INPUT -d "$IP_ADDRESS" -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT -d "$IP_ADDRESS" -p tcp --dport 443 -j ACCEPT
+    # Add iptables rules
+    iptables -A INPUT -d "$IP" -j DROP
+    iptables -I INPUT -d "$IP" -p tcp --dport 80 -j ACCEPT
+    iptables -I INPUT -d "$IP" -p tcp --dport 443 -j ACCEPT
+done
 
 echo "iptables rules applied. Run 'sudo iptables -S' to check."
